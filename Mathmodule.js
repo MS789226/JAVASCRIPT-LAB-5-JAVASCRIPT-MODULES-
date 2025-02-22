77efe6b7c2935cfbcdb14b284c1ea46fe0c15cd2
MS789226 <thyhmbyers@gmail.com>
2025-02-21 22:09:39 -0500

    Create Mathmodule.js

---
+++ Mathmodule.js
@@ -0,0 +1,16 @@
+MATHMODULE.JS
+
+// mathModule.js
+
+// Function to add two numbers
+export function add(a, b) {
+    return a + b;
+}
+
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Default export for the multiply function
+export default multiply;

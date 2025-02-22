24c5c25c564c6fd4a768424f674ce4245d72503a
MS789226 <thyhmbyers@gmail.com>
2025-02-21 22:13:19 -0500

    Create String.js

---
+++ String.js
@@ -0,0 +1,13 @@
+STRINGMODULE.JS
+
+// stringModule.js
+
+// Function to convert a string to lowercase
+export function toLowerCase(str) {
+    return str.toLowerCase();
+}
+
+// Function to convert a string to uppercase
+export function toUpperCase(str) {
+    return str.toUpperCase();
+}

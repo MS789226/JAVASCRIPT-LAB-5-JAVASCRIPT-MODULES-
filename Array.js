189e469dc5484cb270ae9dd08a0521ff0d5cebe5
MS789226 <thyhmbyers@gmail.com>
2025-02-21 22:10:51 -0500

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,12 @@
+ARRAYMODULE.JS
+
+// arrayModule.js
+
+// Function to find the maximum value in an array
+export function findMax(arr) {
+    return Math.max(...arr);
+}
+
+// Function to reverse an array
+export function reverseArray(arr) {
+    return arr.reverse();
